3dd40a9f09168af1645eb0147029b829c94d467a
chinmay4382 <35778975+chinmay4382@users.noreply.github.com>
2018-03-16 10:40:10 +0530

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,9 @@
+from twilio.rest import Client
+account_sid = "ACbfc9d7644a2aff8838126d888976c717"
+auth_token = "8ebd45aa1672b5fd293817fe1a8cc428"
+client = Client(account_sid, auth_token)
+client.api.account.messages.create(
+	to="+917026251115",
+	from_="+18318880945",
+	 body="this msg was sent by twilio.....for MUSK-v1")
+print("message sent")
